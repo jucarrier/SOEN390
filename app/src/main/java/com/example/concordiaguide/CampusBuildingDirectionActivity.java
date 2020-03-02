25f1a9d48e4176bd2a641e024672f1ffa59c6995
Najim Ghafourzadeh <46761237+najimgh@users.noreply.github.com>
2020-03-01 22:06:37 -0500

    Added the CampusBuildingDirectionActivity class

---
+++ app/src/main/java/com/example/concordiaguide/CampusBuildingDirectionActivity.java
@@ -0,0 +1,28 @@
+package com.example.concordiaguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class CampusBuildingDirectionActivity extends AppCompatActivity {
+
+    private static final String TAG = "CampusBuildingDirection";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_info);
+        Log.d(TAG, "onCreate: Starting.");
+    }
+
+    public void goToMainActivity(View view){
+        Log.d(TAG, "onClick: Clicked directionBtn.");
+        Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+
+    }
+}
