e636b9c018f052c5c7cc7f1574225da31d3d8ea8
John <jestrada740@gmail.com>
2020-03-21 09:46:35 -0400

    added ClassSchedule class to store calendar events

---
+++ app/src/main/java/Helpers/ClassSchedule.java
@@ -0,0 +1,26 @@
+package Helpers;
+
+import java.util.ArrayList;
+
+import Models.CalendarEvent;
+
+public class ClassSchedule {
+    //TODO: add a save as JSON method
+    //TODO: add a load from file method
+    //TODO: add a read from calendar method
+    protected ArrayList<CalendarEvent> events;
+
+    public ClassSchedule(ArrayList<CalendarEvent> events) {
+        this.events = events;
+    }
+
+    //TODO: add better duplicated detection - need a .equals method for calendar events?
+    public void addEvent(CalendarEvent event){
+        if(events.contains(event)) System.out.println("Duplicate event");
+        else events.add(event);
+    }
+
+    public ArrayList<CalendarEvent> getEvents(){
+        return this.events;
+    }
+}
