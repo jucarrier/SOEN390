cc8a3a2f5c69a6f61086ed40415e44399d00a527
ggebran <ggebran95@gmail.com>
2020-03-27 19:04:15 -0400

    Added JMSB floor
    Added JMSB 1st floor with functionality.

--- app/src/main/java/Helpers/CampusBuilder.java
+++ app/src/main/java/Helpers/CampusBuilder.java
@@ -56,7 +56,12 @@ public class CampusBuilder {
                 new LatLng(45.497165, -73.579551));
 
         Building jm = new Building(mMap, "John Molson", "1450 Guy St, Montreal, Quebec H3H 0A1", "Description", "JM",
-                new Floor[]{},
+                new Floor[]{
+                        new Floor("1st", 1, R.drawable.ic_mb_1,
+                                new String[]{"MB1.294", "MB1.210", "MB1.338", "MB1.310", "MB1.437", "MB1.301", "MB1.309", "MB1.394", "MB1.335",
+                                        "MB1.315", "MB1.424", "MB1.410", "MB1.426", "MB1.430", "MB1.436", "MB1.435", "MB1.494", "MB1.115",
+                                        "MB1.434", "MB1.132", "MB1.130", "MB1.134", "MB1.299"})
+                },
                 new LatLng(45.495166, -73.579171),
                 new LatLng(45.495222, -73.579113),
                 new LatLng(45.495361, -73.579370),
